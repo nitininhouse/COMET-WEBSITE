b052ff113f4f334feaa8f73b8a0ef8a9d14f42c6
Akshit517 <akshitmandial517@gmail.com>
2025-01-17 21:46:32 +0530

    add: navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,13 @@ module.exports = {
     "./src/**/*.{html,js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'Aquire': ['Aquire', 'sans-serif'],
+        'DmSans': ['DM Sans', 'sans-serif'],
+        'Ysabeau': ['Ysabeau SC', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
